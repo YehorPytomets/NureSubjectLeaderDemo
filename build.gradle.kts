17e958d641ef4582982213bbb287bd6e87ba7726
yehor.pytomets <yehor.pytomets@teamdev.com>
2021-04-14 10:16:25 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    java
+}
+
+group = "ua.nure.leader.demo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "NureSubjectLeaderDemo"
+include("bot")
