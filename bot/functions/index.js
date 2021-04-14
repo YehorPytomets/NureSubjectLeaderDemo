9b82221af5531507c82ae9d0cb23ef105a7d714a
yehor.pytomets <yehor.pytomets@teamdev.com>
2021-04-14 16:07:42 +0300

    Recreate Echo functions

--- bot/functions/index.js
+++ bot/functions/index.js
@@ -1,9 +1,26 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const Telegraf = require('telegraf')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const bot = new Telegraf(functions.config().telegram.token, {
+    telegram: { webhookReply: true },
+})
+
+// error handling
+bot.catch((err, ctx) => {
+    functions.logger.error('[Bot] Error', err)
+    return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
+
+// initialize the commands
+bot.command('/start', (ctx) => ctx.reply('Hello! Send any message and I will copy it.'))
+// copy every message and send to the user
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
+
+// handle all telegram updates with HTTPs trigger
+exports.echoBot = functions.https.onRequest(async (request, response) => {
+    functions.logger.log('Incoming message', request.body)
+    return await bot.handleUpdate(request.body, response).then((rv) => {
+        // if it's not a request from the telegram, rv will be undefined, but we should respond with 200
+        return !rv && response.sendStatus(200)
+    })
+})
